d2a3fe45a2d5fb124b07eabc8ee695d2c8b63db0
Gnaneswari Pudami <gnaneswari.pudami@gmail.com>
2026-01-21 10:34:57 +0000

    Commented password lines

--- Deploying_Microservices/Docker/images-build.sh
+++ Deploying_Microservices/Docker/images-build.sh
@@ -16,10 +16,10 @@ echo "docker images created"
 docker image ls
 
 
-docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8080:8080 notifications-service:1.0
-docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8081:8081 product-service:1.0
-docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8082:8082 useraccount-service:1.0
-docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8083:8083 order-service:1.0
+#docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8080:8080 notifications-service:1.0
+#docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8081:8081 product-service:1.0
+#docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8082:8082 useraccount-service:1.0
+#docker run -d -e AWS_REGION=eu-west-2 -e AWS_ACCESS_KEY_ID=AKIA4MI2KB54JQAEGY4G -e AWS_SECRET_ACCESS_KEY=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 -p 8083:8083 order-service:1.0
 
 docker ps
 
